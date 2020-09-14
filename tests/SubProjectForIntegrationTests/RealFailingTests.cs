7f7b515d1b4f2a36d280258192290d1200fd6da1
rex-ubuntu <rexfleischer@gmail.com>
2020-09-14 16:53:57 -0700

    adding github ci steps for different platforms

---
+++ tests/SubProjectForIntegrationTests/RealFailingTests.cs
@@ -0,0 +1,23 @@
+using GodotXUnitApi;
+using GodotXUnitTest;
+using Xunit;
+
+namespace SubProjectForIntegrationTests
+{
+    /// <summary>
+    /// these tests are required to fail. so i'm putting these inside
+    /// this integration tests sub project so:
+    /// - we can build a CI example of running tests from a subproject
+    /// - we can see an example of what happens when it fails
+    /// </summary>
+    public class RealFailingTests
+    {
+        [GodotFact(Scene = "res://SomeSceneNotFound.tscn")]
+        public void SceneUnableToBeFound()
+        {
+            GDU.Print("this will fail");
+            var scene = GDU.CurrentScene;
+            Assert.Equal(typeof(SomeTestSceneRoot), scene?.GetType());
+        }
+    }
+}
