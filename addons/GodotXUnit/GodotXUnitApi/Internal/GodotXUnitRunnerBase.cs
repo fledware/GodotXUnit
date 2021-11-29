036e5066cb944f830404b95e9890d3a56d862724
Rex Fleischer <rexfleischer@gmail.com>
2021-11-29 16:18:47 -0600

    Rider test runner (#15)
    * WIP to get GodotXUnit to work in rider
    
    * Step towards packing GodotXUnitApi to nuget (#13)
    
    * step towards making GodotXUnitApi nuget package: move logic from GodotTestRunner to GodotXUnitRunner, which sits in GodotXUnitApi
    
    * new csproj format GodotXUnitApi.csproj
    
    * wip towards packing GodotXUnitApi into nuget
    
    * update other csproj files too use net 5 msbuild
    
    * forgotten files
    
    * avoid build problems (access to the same file)
    
    * fix path for loading test assemblies
    
    * move RiderTestRunner inside addons\GodotXUnit\runner
    
    * update the runner image to 3.3.3 (#11)
    
    * update godot plugin version to 0.10.0 (#12)
    
    * trying to fix a new godot error:
    
    ERROR: _load_scripts_metadata: Missing scripts metadata file.
    
    Co-authored-by: Ivan Shakhov <van800@gmail.com>

--- addons/GodotXUnit/GodotXUnitApi/Internal/GodotXUnitRunnerBase.cs
+++ addons/GodotXUnit/GodotXUnitApi/Internal/GodotXUnitRunnerBase.cs
@@ -4,16 +4,70 @@ using System.Reflection;
 using Godot;
 using Newtonsoft.Json;
 using Xunit.Runners;
+using Path = System.IO.Path;
+using Directory = System.IO.Directory;
 
 namespace GodotXUnitApi.Internal
 {
-    public abstract class GodotXUnitRunner : Node2D
+    public abstract class GodotXUnitRunnerBase : Node2D
     {
-        protected abstract Assembly GetTargetAssembly(GodotXUnitSummary summary);
+        protected virtual Assembly GetTargetAssembly(GodotXUnitSummary summary)
+        {
+            // check if we even have a project assembly set
+            if (!ProjectSettings.HasSetting(Consts.SETTING_TARGET_ASSEMBLY))
+                return Assembly.GetExecutingAssembly();
 
-        protected abstract string GetTargetClass(GodotXUnitSummary summary);
+            // get the project and if its the default (the godot project), return executing
+            var targetProject = ProjectSettings.GetSetting(Consts.SETTING_TARGET_ASSEMBLY).ToString();
+            if (string.IsNullOrEmpty(targetProject) || targetProject.Equals(ProjectListing.GetDefaultProject()))
+                return Assembly.GetExecutingAssembly();
 
-        protected abstract string GetTargetMethod(GodotXUnitSummary summary);
+            // if its a custom project target, attempt to just load the assembly directly
+            if (targetProject.Equals(Consts.SETTING_TARGET_ASSEMBLY_CUSTOM_FLAG))
+            {
+                var customDll = ProjectSettings.HasSetting(Consts.SETTING_TARGET_ASSEMBLY_CUSTOM)
+                    ? ProjectSettings.GetSetting(Consts.SETTING_TARGET_ASSEMBLY_CUSTOM).ToString()
+                    : "";
+                if (string.IsNullOrEmpty(customDll))
+                {
+                    summary.AddDiagnostic("no custom dll assembly configured.");
+                    GD.PrintErr("no custom dll assembly configured.");
+                    return Assembly.GetExecutingAssembly();
+                }
+
+                summary.AddDiagnostic($"attempting to load custom dll at: {customDll}");
+                return AppDomain.CurrentDomain.Load(AssemblyName.GetAssemblyName(customDll));
+            }
+
+            // find the project in the project list. if its not there, print error and leave
+            var projectList = ProjectListing.GetProjectInfo();
+            if (!projectList.ContainsKey(targetProject))
+            {
+                GD.PrintErr(
+                    $"unable to find project {targetProject}. expected values: {string.Join(", ", projectList.Keys)}");
+                return Assembly.GetExecutingAssembly();
+            }
+
+            // finally, attempt to load project..
+            var currentDir = Directory.GetCurrentDirectory();
+            var targetAssembly = Path.Combine(currentDir, $".mono/build/bin/Debug/{targetProject}.dll");
+            var name = AssemblyName.GetAssemblyName(targetAssembly);
+            return AppDomain.CurrentDomain.Load(name);
+        }
+
+        protected virtual string GetTargetClass(GodotXUnitSummary summary)
+        {
+            return ProjectSettings.HasSetting(Consts.SETTING_TARGET_CLASS)
+                ? ProjectSettings.GetSetting(Consts.SETTING_TARGET_CLASS)?.ToString()
+                : null;
+        }
+
+        protected virtual string GetTargetMethod(GodotXUnitSummary summary)
+        {
+            return ProjectSettings.HasSetting(Consts.SETTING_TARGET_METHOD)
+                ? ProjectSettings.GetSetting(Consts.SETTING_TARGET_METHOD)?.ToString()
+                : null;
+        }
 
         private ConcurrentQueue<Action<Node2D>> drawRequests = new ConcurrentQueue<Action<Node2D>>();
 
@@ -157,8 +211,8 @@ namespace GodotXUnitApi.Internal
             var location = ProjectSettings.HasSetting(Consts.SETTING_RESULTS_SUMMARY)
                 ? ProjectSettings.GetSetting(Consts.SETTING_RESULTS_SUMMARY).ToString()
                 : Consts.SETTING_RESULTS_SUMMARY_DEF;
-            var file = new File();
-            var result = file.Open(location, File.ModeFlags.Write);
+            var file = new Godot.File();
+            var result = file.Open(location, Godot.File.ModeFlags.Write);
             if (result == Error.Ok)
                 file.StoreString(JsonConvert.SerializeObject(testSummary, Formatting.Indented, WorkFiles.jsonSettings));
             else
